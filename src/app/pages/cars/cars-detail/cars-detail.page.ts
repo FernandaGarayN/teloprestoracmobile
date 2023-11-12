54564b7977cd90e9231b44f390a7c2a9f0cd0483
Fernanda Garay <m.garay@duocuc.cl>
2023-11-11 21:33:24 -0300

    Inicio de proyecto
    Configuración inicial

---
+++ src/app/pages/cars/cars-detail/cars-detail.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-detail',
+  templateUrl: './cars-detail.page.html',
+  styleUrls: ['./cars-detail.page.scss'],
+})
+export class CarsDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
